8d9403ee82ac9f955e4cdd294cf2610d1b823a9e
Wallace Santana <wallacesantanak0@gmail.com>
2025-06-05 12:51:04 -0300

    (faet): linkedListCycle

---
+++ linkedListCycle/main.c
@@ -0,0 +1,14 @@
+bool hasCycle(struct ListNode *head)
+{
+    if (head == NULL)
+        return false;
+    struct ListNode *slow = head, *fast = head->next;
+    while (slow != fast)
+    {
+        if (fast == NULL || fast->next == NULL)
+            return false;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return true;
+}
