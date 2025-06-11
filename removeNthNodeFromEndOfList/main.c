aa953e93fb1d616b13936f87834b95536dbbca1a
Wallace Santana <wallacesantanak0@gmail.com>
2025-06-11 14:36:45 -0300

    (feat): removeNthNode

---
+++ removeNthNodeFromEndOfList/main.c
@@ -0,0 +1,11 @@
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode* fast = head, *slow = head;
+    for (int i = 0; i < n; i++) fast = fast->next;
+    if (!fast) return head->next;
+    while (fast->next) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow->next = slow->next->next;
+    return head;   
+}
