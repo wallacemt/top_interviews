cac542f26c3d385e3bcb6b9e307a09f4da92dd64
Wallace Santana <wallacesantanak0@gmail.com>
2025-06-24 11:27:05 -0300

    (feat): flatteBTToLinkedList

---
+++ flattenBinaryTreeToLinkedList/main.c
@@ -0,0 +1,43 @@
+// Given the root of a binary tree, flatten the tree into a "linked list":
+
+// The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,5,3,4,null,6]
+// Output: [1,null,2,null,3,null,4,null,5,null,6]
+// Example 2:
+
+// Input: root = []
+// Output: []
+// Example 3:
+
+// Input: root = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+void flatten(struct TreeNode* root) {
+    if (!root) return;
+    flatten(root->left);
+    flatten(root->right);
+    struct TreeNode* right = root->right;
+    root->right = root->left;
+    root->left = NULL;
+    struct TreeNode* p = root;
+    while (p->right) p = p->right;
+    p->right = right;    
+}
+
