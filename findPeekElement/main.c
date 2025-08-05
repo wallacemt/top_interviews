c07a6552a9728603c1c934a8d646ea07c9712e98
Wallace Santana <wallacesantanak0@gmail.com>
2025-08-05 13:54:07 -0300

    (feat): findPeekElement

---
+++ findPeekElement/main.c
@@ -0,0 +1,12 @@
+int findPeakElement(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;   
+}
