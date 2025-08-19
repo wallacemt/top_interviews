6dfe94b2d653052953f25b3a42e535cd69f67af4
Wallace Santana <wallacesantanak0@gmail.com>
2025-08-19 12:48:42 -0300

    (feat):singleNumberII

---
+++ singleNumberII/main.c
@@ -0,0 +1,11 @@
+int singleNumber(int* nums, int numsSize) {
+    int ones = 0, twos = 0;
+    for (int i = 0; i < numsSize; i++) {
+        twos |= ones & nums[i];
+        ones ^= nums[i];
+        int threes = ones & twos;
+        ones &= ~threes;
+        twos &= ~threes;
+    }
+    return ones;
+}
